18c1f3bf878bb51ade0494426b4472a196a77643
Sybille Peters <sypets@gmx.de>
2020-01-24 13:30:51 +0100

    [TASK] Use config file for bash scripts

---
+++ config.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# automatic variables
+thisdir=$(dirname $0)
+cd $thisdir
+curdir=$(pwd)
+thisdir=$(pwd)
+
+
+# config
+generateddir=$thisdir/generated-data
+repodir=$generateddir/repos
+phpdir=$thisdir
+

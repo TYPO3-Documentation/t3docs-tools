03abe88cbf98d191152cf229840c2abc934ea68d
Alexander Nitsche <typo3@alexandernitsche.com>
2022-04-30 11:05:36 +0200

    [TASK] Support hosts in /get-repo-branches.php

--- get-repo-branches.php
+++ get-repo-branches.php
@@ -2,38 +2,67 @@
 
 use T3docs\T3docsTools\Configuration;
 use T3docs\T3docsTools\GitHub\GithubRepository;
+use T3docs\T3docsTools\GitLab\GitLabRepository;
 
 $loader = require 'vendor/autoload.php';
 
 function usage()
 {
-    $users = (new Configuration())->getSortedFilteredUsers('all');
-    print("Usage: php get-repo-branches.php [<type>] [<user>] [<token>]\n");
+    $config = new Configuration();
+    $hosts = $config->getSortedFilteredHosts('all');
+    $users = $config->getSortedFilteredUsers('all', 'all');
+    print("Usage: php get-repo-branches.php [<type>] [<host>] [<user>] [<token>]\n");
     print("\n");
     print("Arguments:\n");
     print("   type: Consider all repositories or only those starting with \"TYPO3CMS-\" (all, docs). [default: \"docs\"]\n");
-    print("   user: Consider the repositories of this GitHub user namespace (" . implode(', ', $users) . "), which has to be defined in the /config.yml or /config.local.yml. [default: \"typo3-documentation\"]\n");
-    print("   token: Fetch the repositories using this GitHub API token to overcome GitHub rate limitations. [default: \"\"]\n");
+    print("   host: Consider the repositories of this host (all, " . implode(', ', $hosts) . "), which has to be defined in the /config.yml or /config.local.yml. [default: \"github.com\"]\n");
+    print("   user: Consider the repositories of this user namespace (all, " . implode(', ', $users) . "), which has to be defined in the /config.yml or /config.local.yml. [default: \"github.com:typo3-documentation\"]\n");
+    print("   token: Fetch the repositories using this GitHub / GitLab API token to overcome rate limitations. [default: \"\"]\n");
     exit(1);
 }
 
-if ($argc > 4) {
+if ($argc > 5) {
     usage();
 }
 
 $type = $argv[1] ?? 'docs';
-$user = $argv[2] ?? 'typo3-documentation';
-$token = $argv[3] ?? '';
+$host = $argv[2] ?? 'github.com';
+$user = $argv[3] ?? 'github.com:typo3-documentation';
+$token = $argv[4] ?? '';
 
-$gitRepository = new GithubRepository($token);
-$gitRepository->fetchRepos($user, $type);
-$repos = $gitRepository->getNames();
+$config = new Configuration();
+$users = $config->getSortedFilteredUsers($host, $user);
 
-foreach($repos as $repo) {
-    print("$repo\n");
+$repos = [];
+foreach ($users as $userIdentifier) {
+    list($host, $user) = explode(':', $userIdentifier, 2);
+    if ($config->getTypeOfHost($host) === Configuration::HOST_TYPE_GITHUB) {
+        $gitRepository = new GithubRepository($host, $token);
+        $gitRepository->fetchRepos($user, $type);
+        $repos[$userIdentifier] = $gitRepository->getNames();
+    } elseif ($config->getTypeOfHost($host) === Configuration::HOST_TYPE_GITLAB) {
+        $gitRepository = new GitLabRepository($host, $token);
+        $gitRepository->fetchRepos($user, $type);
+        $repos[$userIdentifier] = $gitRepository->getNames();
+    }
+}
 
-    $branches = $gitRepository->fetchBranchNamesOfRepo($user, $repo);
-    foreach ($branches as $branch) {
-        print("  $branch \n");
+foreach ($repos as $userIdentifier => $reposByUser) {
+    list($host, $user) = explode(':', $userIdentifier, 2);
+    foreach ($reposByUser as $repo) {
+        print("{$userIdentifier}:{$repo}\n");
+
+        $branches = [];
+        if ($config->getTypeOfHost($host) === Configuration::HOST_TYPE_GITHUB) {
+            $gitRepository = new GithubRepository($host, $token);
+            $branches = $gitRepository->fetchBranchNamesOfRepo($user, $repo);
+        } elseif ($config->getTypeOfHost($host) === Configuration::HOST_TYPE_GITLAB) {
+            $gitRepository = new GitLabRepository($host, $token);
+            $branches = $gitRepository->fetchBranchNamesOfRepo($user, $repo);
+        }
+
+        foreach ($branches as $branch) {
+            print("  $branch \n");
+        }
     }
 }
